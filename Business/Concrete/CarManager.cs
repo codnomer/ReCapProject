26fa90f6375d1e840a28121ceba2d15b6b220617
Ömer Demirhan <05170000461@ogrenci.ege.edu.tr>
2021-03-21 16:14:50 +0300

    rent a car project

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _ıcar;
+
+        public CarManager(ICarDal ıcar)
+        {
+            _ıcar = ıcar;
+        }
+
+        public void Add(Car car)
+        {
+            _ıcar.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _ıcar.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _ıcar.GetAll();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _ıcar.GetById(id);
+        }
+
+        public void Update(Car car)
+        {
+            _ıcar.Update(car);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=100.000,Description="Şehir İçi konfor için ideal",ModelYear=2017},
+                new Car{Id=2,BrandId=2,ColorId=2,DailyPrice=200.000,Description="Hava atmak istiyorsanız bu arabayı seçmelisiniz",ModelYear=1990},
+                new Car{Id=3,BrandId=3,ColorId=3,DailyPrice=150.000,Description="Hızlı ve güvenli",ModelYear=2012}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+
+        }
+
+        
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
